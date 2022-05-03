d478e01251b1ac8fea8e51d06a748aaf6be86361
pentcle <pentacle@pentacle.xyz>
2022-05-03 20:49:16 +0100

    margins

--- src/pages/main.js
+++ src/pages/main.js
@@ -5,23 +5,21 @@ import logo from '../images/yearn-logo.svg';
 import {Link} from "react-router-dom";
 
 export const Main = () => {
-  return (
-    <div>
-        <Layout>
-            <main>
-                <article className={"vertical-center-container"}>
-                    <p className={"text-large vertical-center"}>
-                        YFI story
-                    </p>
-                    <Link to={"/thebluepill/en"}>the blue pill - english</Link>
-                    <Link to={"/thebluepill/sp"}>the blue pill - spanish</Link>
-                    <Link to={"/thebluepill/hi"}>the blue pill - hindi</Link>
-                    <Link to={"/thebluepill/po"}>the blue pill - portugese</Link>
-                    <Link to={"/thebluepill/tu"}>the blue pill - turkish</Link>
-                    <Link to={"/images"}>image library</Link>
-                </article>
-            </main>
-      </Layout>
-    </div>
-  );
+    return (<div>
+            <Layout>
+                <main>
+                    <article className={"vertical-center-container"}>
+                        <p className={"text-large vertical-center"}>
+                            YFI story
+                        </p>
+                        <Link className={"margin-top-2"} to={"/thebluepill/en"}>the blue pill - english</Link>
+                        <Link to={"/thebluepill/sp"}>the blue pill - spanish</Link>
+                        <Link to={"/thebluepill/hi"}>the blue pill - hindi</Link>
+                        <Link to={"/thebluepill/po"}>the blue pill - portugese</Link>
+                        <Link to={"/thebluepill/tu"}>the blue pill - turkish</Link>
+                        <Link className={"margin-top-2"} to={"/images"}>image library</Link>
+                    </article>
+                </main>
+            </Layout>
+        </div>);
 };
