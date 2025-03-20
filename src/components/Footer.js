f55047794e5888dfe17d831ef45fa3dbe3ca464c
rossgalloway <58150151+rossgalloway@users.noreply.github.com>
2025-03-20 15:33:13 -0400

    Chore: Update Footer.js (#12)
    * Chore: Update Footer.js
    
    -Update twitter/X, github, website.
    -Remove calendar link
    
    * Update IconYearnFooter.js
    
    update to use new yearn logo
    
    * fix: sizing
    
    * fix: sizing
    
    * fix: sizing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,10 +8,9 @@ import IconCalendar from "./icons/IconCalendar";
 class Footer extends Component {
     render() {
         return (<footer>
-            <a className="icon-link" href={'https://twitter.com/iearnfinance'} target={'_blank'} rel={'noreferrer'}><span>Twitter</span><IconTwitter/></a>
-            <a className="icon-link" href={'https://github.com/pentcle/yearn-fun'} target={'_blank'} rel={'noreferrer'}><span>Github</span><IconGithub/></a>
-            <a className="icon-link" href={'https://yearn.finance'} target={'_blank'} rel={'noreferrer'}><span>Yearn</span><IconYearnFooter/></a>
-            <a className="icon-link" href={'https://calendar.google.com/calendar/u/0?cid=ZWgybmRuZm03cHQ0NjliMDRibnU4cGEzYWNAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ'} target={'_blank'} rel={'noreferrer'}><span>Calendar</span><IconCalendar/></a>
+            <a className="icon-link" href={'https://x.com/yearnfi'} target={'_blank'} rel={'noreferrer'}><span>Twitter</span><IconTwitter/></a>
+            <a className="icon-link" href={'https://github.com/yearn'} target={'_blank'} rel={'noreferrer'}><span>Github</span><IconGithub/></a>
+            <a className="icon-link" href={'https://yearn.fi'} target={'_blank'} rel={'noreferrer'}><span>Yearn</span><IconYearnFooter/></a>
         </footer>);
     }
 }
